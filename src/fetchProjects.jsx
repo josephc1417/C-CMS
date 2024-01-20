80a0fed3e427a2b068effcc5134bde76418490d4
Joseph Clay <Josephc1417@gmail.com>
2024-01-19 23:18:12 -0600

    AT error

--- src/fetchProjects.jsx
+++ src/fetchProjects.jsx
@@ -4,9 +4,8 @@ import { useState, useEffect } from "react";
 const client = createClient({
   space: 'kxu62xdyg59p',
   environment: 'master',
-  accessToken: import.meta.env.VITE_API_KEY,
+  accessToken: 'ZgPvqpleE7uEAe4Q0v6xGmDxB3mtp99znLuaY_itVyk',
 });
-
 export const useFetchProjects = () => {
   const [loading, setLoading] = useState(true);
   const [projects, setProjects] = useState([]);
@@ -23,7 +22,7 @@ export const useFetchProjects = () => {
       setProjects(projects);
       setLoading(false);
     } catch (error) {
-      console.log(error);
+      console.error(error);
       setLoading(false);
     }
   };
